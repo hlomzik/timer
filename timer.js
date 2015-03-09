de52c13954036e39b7dda4445dd9e8426002a578
hlomzik <hlomzik@gmail.com>
2015-03-09 20:27:10 +0300

    first implementation of set-/clearTimeout() - just a wrapper around standard functions; and stubs for set-/clearInterval()

--- timer.js
+++ timer.js
@@ -16,6 +16,9 @@ window.Timer = function() {
 		this.elapsed = 0;
 		this.checkpoint = 0;
 		this.started = false;
+		
+		this.timeouts = [];
+		this.intervals = [];
 	};
 	
 	Timer.prototype = {
@@ -37,6 +40,27 @@ window.Timer = function() {
 		},
 		getElapsedTime: function() {
 			return this.elapsed + this.getSessionTime();
+		},
+		setTimeout: function(f, time) {
+			var uid = 0;
+			// @todo shim for Function.bind()
+			var wrapper = (function() {
+				setTimeout(f, 0);
+				this.clearTimeout(uid);
+			}).bind(this);
+			uid = setTimeout(wrapper, time);
+			this.timeouts[uid] = [ wrapper, time, this.getElapsedTime() ];
+			return uid;
+		},
+		clearTimeout: function(uid) {
+			delete this.timeouts[uid];
+			clearTimeout(uid);
+		},
+		setInterval: function() {
+			// @todo implement
+		},
+		clearInterval: function(uid) {
+			// @todo implement
 		}
 	};
 	
